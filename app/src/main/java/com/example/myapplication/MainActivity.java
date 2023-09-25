53b84aa8173815f75c8a509413c656c0a221bc37
victo <vam00022@red.ujaen.es>
2023-09-25 19:15:02 +0300

    Text change button functionality

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +16,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void changeText(View view) {
+        TextView text = (TextView) findViewById(R.id.textView);
+        if (text.getText() != "New Text") {
+            text.setText("New Text");
+            Toast.makeText(this, "Succes", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "The text has been changed", Toast.LENGTH_LONG).show();
+        }
     }
 }

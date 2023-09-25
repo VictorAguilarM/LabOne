ccdd41a3bef1feea0fa17dd9f4299b074b2e7a45
victo <vam00022@red.ujaen.es>
2023-09-25 20:00:05 +0300

    Color change button functionality

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,6 +13,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    int cont=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +28,36 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Succes", Toast.LENGTH_LONG).show();
         } else {
             Toast.makeText(this, "The text has been changed", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void changeColor(View view){
+        TextView text = (TextView) findViewById(R.id.textView);
+        switch (cont){
+            case 1:
+                text.setTextColor(Color.RED);
+                cont++;
+                break;
+
+            case 2:
+                text.setTextColor(Color.BLUE);
+                cont++;
+                break;
+
+            case 3:
+                text.setTextColor(Color.GREEN);
+                cont++;
+                break;
+
+            case 4:
+                text.setTextColor(Color.MAGENTA);
+                cont++;
+                break;
+
+            case 5:
+                text.setTextColor(Color.BLACK);
+                cont=1;
+                break;
         }
     }
 }
